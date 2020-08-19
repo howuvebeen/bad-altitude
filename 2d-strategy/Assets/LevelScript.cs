7ce4ec5bdf7075841c668b5874b7fba7c2816e44
howuvebeen <yubinheo11@gmail.com>
2020-08-19 23:57:43 +0900

    Added Levels scene (needs debugging)

---
+++ 2d-strategy/Assets/LevelScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class LevelScript : MonoBehaviour
+{
+    
+    public Button levelOneButton, levelTwoButton, levelThreeButton;
+
+    // Update is called once per frame
+    void Start()
+    {
+        levelOneButton.interactable = true;
+        levelTwoButton.interactable = false;
+        levelThreeButton.interactable = false;
+    }
+
+    public void levelToLoad(int level)
+    {
+        SceneManager.LoadScene(level);
+    }
+}
